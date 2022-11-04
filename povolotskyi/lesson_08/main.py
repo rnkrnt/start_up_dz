589538b20347777f2b70a4eaad2f3c7fafa7e12c
Dmytro Povolotskyi <dmitriiiyp@gmail.com>
2022-11-04 21:44:09 +0200

    Commit 4. main, create branch test

--- povolotskyi/lesson_08/main.py
+++ povolotskyi/lesson_08/main.py
@@ -2,3 +2,4 @@ def maine_function():
     print("Commit 1. master")
     print("Commit 2. master")
     print("Commit 3. main, rename maine.py -> main.py")
+    print("Commit 4. main, create branch test")
