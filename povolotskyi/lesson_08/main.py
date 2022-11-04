71ab977bc49bfec35b07a399f9cc355cb4714609
Dmytro Povolotskyi <dmitriiiyp@gmail.com>
2022-11-04 21:51:01 +0200

    Commit 5. test

--- povolotskyi/lesson_08/main.py
+++ povolotskyi/lesson_08/main.py
@@ -2,3 +2,6 @@ def maine_function():
     print("Commit 1. master")
     print("Commit 2. master")
     print("Commit 3. main, rename maine.py -> main.py")
+ 
+ def test_function():
+     print("Commit 5. test") 
