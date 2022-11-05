52e3db1b1d570707102ad1920f1facb2adc959ed
Dmytro Povolotskyi <dmitriiiyp@gmail.com>
2022-11-05 19:05:33 +0200

    Commit 12. test_2, create test_2_py_pycharm_function

--- povolotskyi/lesson_08/py_charm.py
+++ povolotskyi/lesson_08/py_charm.py
@@ -2,3 +2,6 @@ def py_pycharm_function():
     print("Commit 8. main, create py_charm.py")
     print("Commit 9. main, change py_charm.py")
     print("Commit 10. main, change_2 py_charm.py")
+
+def test_2_py_pycharm_function():
+    print("Commit 12. test_2, create test_2_py_pycharm_function")
